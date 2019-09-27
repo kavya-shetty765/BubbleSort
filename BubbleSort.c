2c8ebc0e9ac9002443d4521ed6fdf935e740a85c
KAVYA R SHETTY <53563183+kavya-shetty765@users.noreply.github.com>
2019-09-27 16:26:40 +0530

    Add files via upload

---
+++ BubbleSort.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+
+int main()
+
+{
+
+int n; 
+scanf("%d",&n);
+
+int arr[n];
+int i;
+
+
+for(i=0;i<n;i++)
+{
+
+scanf("%d",&arr[i]);
+
+}
+
+
+int j,k,count=0; 
+
+for(j=0;j<n-1;j++)
+{
+
+for(k=0;k<i-j-1;k++)
+{
+
+if(arr[k]>arr[k+1])
+{
+
+int temp,a;
+
+temp=arr[k];
+
+arr[k]=arr[k+1];
+
+arr[k+1]=temp;
+
+count++;
+
+}
+
+}
+
+}
+
+printf("%d\n",count);
+
+
+return 0;
+
+}
